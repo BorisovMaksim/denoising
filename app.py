3b4a0f4230656f57fc34d20ad538e443560d70b4
BorisoMaksim <maksim.borisov.2013@gmail.com>
2023-05-21 21:26:46 +0300

    update app

--- app.py
+++ app.py
@@ -10,6 +10,20 @@ import torchaudio
 import yaml
 
 
+def denoising_transform(audio, model):
+    src_path = Path("cache_wav/original/{}.wav".format(str(uuid.uuid4())))
+    tgt_path = Path("cache_wav/denoised/{}.wav".format(str(uuid.uuid4())))
+    src_path.parent.mkdir(exist_ok=True, parents=True)
+    tgt_path.parent.mkdir(exist_ok=True, parents=True)
+    (ffmpeg.input(audio)
+     .output(src_path.as_posix(), acodec='pcm_s16le', ac=1, ar=22050)
+     .run()
+     )
+    wav, rate = torchaudio.load(audio)
+    reduced_noise = model.predict(wav)
+    torchaudio.save(tgt_path, reduced_noise, rate)
+    return tgt_path
+
 
 def run_app(model_filename, config_filename):
     model_path = hf_hub_download(repo_id="BorisovMaksim/demucs", filename=model_filename)
@@ -20,37 +34,25 @@ def run_app(model_filename, config_filename):
     checkpoint = torch.load(model_path, map_location=torch.device('cpu'))
     model.load_state_dict(checkpoint['model_state_dict'])
 
-    def denoising_transform(audio):
-        # Path(__file__).parent.resolve()
-        src_path = Path("cache_wav/original/{}.wav".format(str(uuid.uuid4())))
-        tgt_path = Path("cache_wav/denoised/{}.wav".format(str(uuid.uuid4())))
-        src_path.parent.mkdir(exist_ok=True, parents=True)
-        tgt_path.parent.mkdir(exist_ok=True, parents=True)
-        (ffmpeg.input(audio)
-         .output(src_path.as_posix(), acodec='pcm_s16le', ac=1, ar=22050)
-         .run()
-         )
-        wav, rate = torchaudio.load(audio)
-        reduced_noise = model.predict(wav)
-        torchaudio.save(tgt_path, reduced_noise, rate)
-        return tgt_path
-
-    demo = gr.Interface(
-        fn=denoising_transform,
+    interface_demucs = gr.Interface(
+        fn=lambda x: denoising_transform(x, model),
         inputs=gr.Audio(label="Source Audio", source="microphone", type='filepath'),
-        outputs=gr.Audio(label="Target Audio", type='filepath'),
-        examples=[
-            ["testing/wavs/p232_071.wav"],
-            ["testing/wavs/p232_284.wav"],
-        ],
-        title="Denoising"
+        outputs=gr.Audio(label="Demucs", type='filepath')
     )
+    interface_spectral_gating = gr.Interface(
+        fn=lambda x: denoising_transform(x, SpectralGating()),
+        inputs=gr.Audio(label="Source Audio", source="microphone", type='filepath'),
+        outputs=gr.Audio(label="Spectral Gating", type='filepath')
+    )
+    gr.Parallel(interface_demucs, interface_spectral_gating,
+                title="Denoising",
+                examples=[
+                    ["testing/wavs/p232_071.wav"],
+                    ["testing/wavs/p232_284.wav"],
+                ]).launch()
 
-
-    demo.launch()
 
 if __name__ == "__main__":
     model_filename = "original_sr/Demucs_original_sr_epoch3.pt"
     config_filename = "original_sr/config.yaml"
     run_app(model_filename, config_filename)
-
