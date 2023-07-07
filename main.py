debd4f4fad3720094c094216efb5e0cf4f7ae4e8
Borisov Maksim <44704968+BorisovMaksim@users.noreply.github.com>
2023-07-08 00:36:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import hydra
 from omegaconf import DictConfig
 from train import train
 
-
+# Parsing hydra config
 @hydra.main(version_base=None, config_path="conf", config_name="config")
 def main(cfg: DictConfig):
     train(cfg)
